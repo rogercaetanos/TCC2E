7b4b3433deb72d9746f3ab55ee7e14b20b1f7447
Rogério Caetano <rogercaetanos@gmail.com>
2026-06-18 11:33:45 +0000

    Criado api spring boot

---
+++ apipetlove/src/main/java/com/itb/inf2em/apipetlove/ApipetloveApplication.java
@@ -0,0 +1,13 @@
+package com.itb.inf2em.apipetlove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApipetloveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApipetloveApplication.class, args);
+	}
+
+}
---
+++ apipetlove/src/test/java/com/itb/inf2em/apipetlove/ApipetloveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itb.inf2em.apipetlove;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApipetloveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
